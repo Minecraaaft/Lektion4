dc77a63a3db81dd953f920eb8159c782d0a70d14
Christian Merithz <cunnn98@gmail.com>
2019-09-27 10:30:35 +0200

    case ændring på sko, og ny string variabel

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        String x="taske" ; String ms;
+        String x= "sko"; String ms;
         switch (x) {
             case "taske": ms = "I love Cock, i mean chicken"; System.out.print(ms + " ");
                 break;
-            case "sko":  ms = "Februar"; System.out.print(ms + " ");
-
+            case "sko":  ms = "Mo giver en tur i kareten for en tyrkerarm"; System.out.print(ms + " ");
+                break;
             case "bukser":  ms = "March"; System.out.print(ms + " ");
                 break;
             case "trøje": ms = "April"; System.out.print(ms + " ");
